9cbb830cc356112f063a0ad504bf50e4ccc730fb
James Meyer <meyer.james@gmail.com>
2015-05-16 22:15:21 -0600

    Test push

---
+++ Chore App/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jamesmeyer
+ * Date: 5/11/15
+ * Time: 3:52 PM
+ */
+
+?>
+
+
+<html doctype="html">
+<head>
+
+</head>
+
+<body>
+    <h2><?php echo getdate(); ?></h2>
+    <p>Here is what you need to do today:</p>
+</body>
+</html>
